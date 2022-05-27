0fad64207e6cf4679b062f31c00f1f3b4407094d
rlaehdud8282 <rlaehdud8282@nate.com>
2022-05-27 18:18:34 +0900

    day15

---
+++ jsworks/day15/js/flower.js
@@ -0,0 +1,21 @@
+
+function showText(){
+    //텍스트 보이기
+   let text = document.getElementById('detail');
+   text.style.display = "block";
+
+   //보기 버튼 숨기기
+  let button = document.getElementById('show')
+    button.style.display = "none";
+}
+
+
+function hideText(){
+    //텍스트는 숨기기
+    let text = document.getElementById('detail');
+    text.style.display = "none";
+
+    //보기 버튼 보이기
+    let button = document.getElementById('show')
+    button.style.display = "block";
+}
